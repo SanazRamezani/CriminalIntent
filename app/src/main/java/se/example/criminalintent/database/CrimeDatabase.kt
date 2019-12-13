ca062d2eb3761070ca80d021fa319ea97a6be855
Sanaz Ramezani <sanaz.ramezani@cevt.se>
2019-12-13 16:45:16 +0100

    Chapter eleven: ROOM database added

---
+++ app/src/main/java/se/example/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,14 @@
+package se.example.criminalintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import se.example.criminalintent.model.Crime
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverter::class)
+abstract class CrimeDatabase : RoomDatabase() {
+
+    abstract fun crimeDao(): CrimeDao
+
+}
--- app/src/main/java/se/example/criminalintent/model/Crime.kt
+++ app/src/main/java/se/example/criminalintent/model/Crime.kt
@@ -1,9 +1,12 @@
 package se.example.criminalintent.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Crime(
-    val id: UUID = UUID.randomUUID(),
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String = "",
     val date: Date = Date(),
     var isSolved: Boolean = false)
